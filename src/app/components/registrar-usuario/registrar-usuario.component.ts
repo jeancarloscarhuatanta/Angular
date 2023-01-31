c8ddac9b6cf9956dc8bd327bb2ea9a4a1edeb94a
chenaoh <cdhenao@gmail.com>
2023-01-30 22:43:14 -0500

    login

---
+++ src/app/components/registrar-usuario/registrar-usuario.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { FirebaseCodeErrorService } from 'src/app/services/firebase-code-error.service';
+
+@Component({
+  selector: 'app-registrar-usuario',
+  templateUrl: './registrar-usuario.component.html',
+  styleUrls: ['./registrar-usuario.component.css'],
+})
+export class RegistrarUsuarioComponent implements OnInit {
+  registrarUsuario: FormGroup;
+  loading: boolean = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private afAuth: AngularFireAuth,
+    private toastr: ToastrService,
+    private router: Router,
+    private firebaseError: FirebaseCodeErrorService
+  ) {
+    this.registrarUsuario = this.fb.group({
+      nombre: ['', [Validators.required]],
+      apellido: ['', [Validators.required]],
+      dni: ['', [Validators.required]],
+      direccion: ['', [Validators.required]],
+      celular: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      repetirPassword: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {}
+
+  registrar() {
+    const nombre = this.registrarUsuario.value.nombre;
+    const apellido = this.registrarUsuario.value.apellido;
+    const email = this.registrarUsuario.value.email;
+    const dni = this.registrarUsuario.value.dni;
+    const direccion = this.registrarUsuario.value.direccion;
+    const celular = this.registrarUsuario.value.celular;
+    const password = this.registrarUsuario.value.password;
+    const repetirPassowrd = this.registrarUsuario.value.repetirPassword;
+
+    console.log(this.registrarUsuario);
+    if (password !== repetirPassowrd) {
+      this.toastr.error(
+        'Las contraseñas ingresadas deben ser las mismas',
+        'Error'
+      );
+      return;
+    }
+
+    this.loading = true;
+    this.afAuth
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        this.verificarCorreo();
+      })
+      .catch((error) => {
+        this.loading = false;
+        this.toastr.error(this.firebaseError.codeError(error.code), 'Error');
+      });
+  }
+
+  verificarCorreo() {
+    this.afAuth.currentUser
+      .then((user) => user?.sendEmailVerification())
+      .then(() => {
+        this.toastr.info(
+          'Le enviamos un correo electronico para su verificacion',
+          'Verificar correo'
+        );
+        this.router.navigate(['/login']);
+      });
+  }
+}
